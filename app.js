e2e93854418a604339bd90b559504d7994569708
“alerenna” <alessandrorennafm@gmail.com>
2025-03-13 21:13:27 +0800

    Add API route for posts managing

--- app.js
+++ app.js
@@ -12,5 +12,33 @@ app.get('/', (req, res) => {
     res.send('Welcome to out server')
 })
 
+//Posts route
 
+//Index
+app.get('/api/v1/posts', (req,res) => {
+    res.send('Return all posts here')
+})
 
+//Show
+app.get('/api/v1/post/:id', (req,res) => {
+    res.send(`Return post with id: ${req.params.id}`)
+})
+
+//Store
+app.post('/api/v1/posts', (req,res) => {
+    res.send('Add a new post')
+})
+
+//Update
+app.put('/api/v1/post/:id', (req,res) => {
+    res.send(`Update post with id: ${req.params.id}`)
+})
+//Modify
+app.patch('/api/v1/post/:id', (req,res) => {
+    res.send(`Modify post with id: ${req.params.id}`)
+})
+
+//Destroy
+app.delete('/api/v1/post/:id', (req,res) => {
+    res.send(`Delete post with id: ${req.params.id}`)
+})
